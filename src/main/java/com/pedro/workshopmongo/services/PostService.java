e7c45a9f9fd5b76a3846f30c40586b0a8178783a
Pedro Henrique <phsr94@gmail.com>
2020-10-23 20:16:49 -0300

    Consulta com vários critérios

--- src/main/java/com/pedro/workshopmongo/services/PostService.java
+++ src/main/java/com/pedro/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.pedro.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,10 +20,12 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
-	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 }
