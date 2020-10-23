74386bfbcb874dffb6e7ddc14b04667065d48560
Pedro Henrique <phsr94@gmail.com>
2020-10-23 13:23:07 -0300

    Operacao e instanciacao da base de dados

--- src/main/java/com/pedro/workshopmongo/service/UserService.java
+++ src/main/java/com/pedro/workshopmongo/service/UserService.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedro.workshopmongo.domain.User;
-import com.pedro.workshopmongo.domain.repository.UserRespository;
+import com.pedro.workshopmongo.domain.repository.UserRepository;
 
 @Service
 public class UserService {
 	@Autowired
-	private UserRespository repo;
+	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
